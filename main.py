faaf001e1ccd22a87c80463a8c42e166c94834b1
Flex-fb <smp_n@rambler.ru>
2025-06-19 17:22:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
+import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет, сладкий, хочешь расслабиться и получать незабываемые удовольствия?")
 
-if __name__ == '__main__':
-    app = ApplicationBuilder().token(os.environ['BOT_TOKEN']).build()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
